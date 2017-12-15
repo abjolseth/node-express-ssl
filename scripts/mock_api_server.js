0a60ce9f78283f80d367166816910b54d60d3b31
Bjølseth, Are <are.bjolseth@nav.no>
2017-12-15 10:55:11 +0100

    Tok i bruk cors() pakken.

--- scripts/mock_api_server.js
+++ scripts/mock_api_server.js
@@ -6,12 +6,15 @@ var http = require('http');
 var https = require('https');
 var fs = require('fs');
 
+const HTTP_PORT  = 3002;
+const HTTPS_PORT = 8443;
+
 var app = express();
-console.log('cors', cors());
+//console.log('cors', cors());
 //app.use(cors); // Use cors for all origins
 
-http.createServer(app).listen(3002, function () {
-  console.log('CORS-enabled web server listening on port 8000');
+http.createServer(app).listen(HTTP_PORT, function () {
+  console.log('CORS-enabled web server listening on port '+HTTP_PORT);
 });
 
 var cwd = process.cwd();
@@ -25,58 +28,72 @@ var sslOptions = {
   cert: fs.readFileSync(scripts+'/cert.pem'),
   passphrase: 'melosys'
 };
-https.createServer(sslOptions, app).listen(8443);
+https.createServer(sslOptions, app).listen(HTTPS_PORT, function () {
+  console.log('CORS-enabled HTTPS web server listening on port '+HTTPS_PORT);
+});
 
 var router = express.Router();
 
-var whitelist = ['localhost'];
+
+var whitelist = ['http://localhost:3000', 'http://10.33.46.179'];
 var corsOptions = {
   origin: function (origin, callback) {
     console.log('origin:', origin);
-    if (whitelist.indexOf(origin) !== -1) {
+    //if (whitelist.indexOf(origin) !== -1) {
+    if (whitelist.includes(origin)) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
     }
   },
   methods: ['GET','POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'Accept-Charset'],
   credentials: true,
   maxAge: 3600,
   preflightContinue: true
 };
 
-var issue2options = {
-  origin: 'localhost', //true== CORS, // Bool | String | RegExp | Array
-  methods: ['GET','POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  maxAge: 3600,
-  preflightContinue: true
-};
-var defaultCorsConfig = {
-  "origin": "*",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-  "preflightContinue": false,
-  "optionsSuccessStatus": 204
-};
 
-
-//, cors(corsOptions)
-router.get('/', function (req, res) {
+router.get('/', cors(corsOptions), function (req, res) {
   hello = {'Hello': 'World'};
   return res.json(hello);
 });
 
 const corsOpts = {
-  origin: 'http://127.0.0.1:3000',
+  origin: 'http://localhost:3000',
   methods: ['GET','POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
   credentials: true,
   maxAge: 3600,
   preflightContinue: true
 };
+
+function reqInfo(req) {
+  console.log('req.baseUrl', req.baseUrl);
+  console.log('req.body', req.body);
+  console.log('req.cookies', req.cookies);
+  console.log('req.fresh', req.fresh);
+  console.log('req.hostname', req.hostname);
+  console.log('req.ip', req.ip);
+  console.log('req.method', req.method);
+  console.log('req.originalUrl', req.originalUrl);
+  console.log('req.params', req.params);
+  console.log('req.path', req.path);
+
+  console.log('req.accepts([\'json\',\'text\'])=>',req.accepts('application/json'));
+  console.log('req.acceptsCharsets(\'UTF-8\')=>',req.acceptsCharsets('UTF-8'));
+  //console.log('req.get(\'Content-Type\')=>', req.get('content-type')); // Only when PUT,POST
+  console.log('req.get(\'Origin\')=>', req.get('Origin'));
+}
+//, cors(corsOpts)
+router.options('/saksbehandler', cors(corsOptions), function (req, res, next) {
+  console.log('\nOPTIONS/saksbehandler');
+  reqInfo(req);
+  next();
+});
 router.get('/saksbehandler', cors(corsOptions), function (req, res) {
+  console.log('\nGET/saksbehandler');
+  reqInfo(req);
   try {
     const saksbehandlere =  JSON.parse(fs.readFileSync("./scripts/mock_data/saksbehandler.json", "utf8"));
     // return a random sakbehandler from list of sakbehandlere
@@ -88,3 +105,4 @@ router.get('/saksbehandler', cors(corsOptions), function (req, res) {
 
 
 app.use('/api', router);
+app.use('/melosys/api', router);
